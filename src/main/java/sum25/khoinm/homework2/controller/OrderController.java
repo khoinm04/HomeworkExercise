6c64ae8dcff73178f9886a14f263e760d4463523
khoinm04 <khoiminhnguyen2k4@gmail.com>
2025-07-19 11:25:49 +0700

    update homepage

--- src/main/java/sum25/khoinm/homework2/controller/OrderController.java
+++ src/main/java/sum25/khoinm/homework2/controller/OrderController.java
@@ -3,6 +3,7 @@ package sum25.khoinm.homework2.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sum25.khoinm.homework2.pojo.*;
@@ -20,7 +21,7 @@ public class OrderController {
     private OrderDetailService orderDetailService;
 
     @GetMapping("/checkout")
-    public String checkout(HttpSession session) {
+    public String checkout(HttpSession session, Model model) {
         if (session.getAttribute("account") == null) {
             return "redirect:/login"; // Redirect to login if not authenticated
         }
@@ -49,6 +50,7 @@ public class OrderController {
                     ))
                     .toList());
 
+            model.addAttribute("order", order);
             return "confirmation"; // Redirect to confirmation page after successful checkout
         }catch (Exception e){
             e.printStackTrace();
